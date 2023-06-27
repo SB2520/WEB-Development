3a9fd80f70c31ad7f12472b4d1c357994e24aac1
Swagat <swagatswaroop@gmail.com>
2023-06-27 09:06:51 +0530

    Completing Java Script Fundamentals

--- Javascript/BasicVariables.js
+++ Javascript/BasicVariables.js
@@ -4,3 +4,14 @@ a = true;
 console.log(a);
 console.log(b);
 console.log("Swagat Swaroop :",a,b);
+// For Infinity
+z = 1/0;
+console.log(z);
+//Not A Number
+var c = parseInt("aaaa");
+console.log(c);
+//We can give null values AS WELL and undefined values
+var l = null;
+console.log(l);
+var k;
+console.log(k);
