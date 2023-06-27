a1f7574cd26dadb487db757f723f7eb6d214400e
Swagat <swagatswaroop@gmail.com>
2023-06-27 20:10:52 +0530

    Basic Functions

---
+++ FunctionsInJS.js
@@ -0,0 +1,12 @@
+function showAlert(){ //Non Parametrized Function
+    console.log("Hey!!");
+}
+
+function add(a,b){  //Parametrized Function 
+    return a+b;
+}
+
+a = 4;
+b = 5;
+console.log(add(a,b));
+showAlert();
