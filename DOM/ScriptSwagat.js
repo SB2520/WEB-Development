dad5b3b3f5130e0daa50ece20a16164d4e8104a2
Swagat <swagatswaroop@gmail.com>
2023-07-26 20:07:03 +0530

    Done with some components

--- DOM/ScriptSwagat.js
+++ DOM/ScriptSwagat.js
@@ -7,3 +7,21 @@ p.addEventListener('mouseover', function(){
 p.addEventListener('mouseout', function(){
     console.log('MouseOut');
 });
+
+var z = document.getElementById('Swagat');
+
+z.addEventListener('keypress', function(){
+    console.log('Keypress');
+});
+
+var s = document.getElementById('Swagat');
+
+s.addEventListener('keydown', function(event){
+    console.log('KeyDown', event.code);
+});
+
+var l = document.getElementById('Swagat');
+
+l.addEventListener('keydown', function(event){
+    console.log('KeyDown', event.keyCode);
+});
