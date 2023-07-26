d5079529de643be9b552d81c604041ccfa976267
Swagat <swagatswaroop@gmail.com>
2023-07-26 19:28:09 +0530

    Doing Dome

---
+++ DOM/ScriptSwagat.js
@@ -0,0 +1,9 @@
+var p = document.getElementsByClassName('cont')[0];
+
+p.addEventListener('mouseover', function(){
+    console.log('MouseOver');
+});
+
+p.addEventListener('mouseout', function(){
+    console.log('MouseOut');
+});
