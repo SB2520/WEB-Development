221600229f5cf86661f581e6966d2866725add22
Swagat <swagatswaroop@gmail.com>
2023-10-26 10:13:34 +0530

    Taken out image from JSON

--- AJAX/ajax.js
+++ AJAX/ajax.js
@@ -6,6 +6,10 @@ let fetchdogphoto = ()=>{
     //on receiving the resposne
     xhrReq.onload = ()=>{
         console.log(xhrReq.response);
+        let a = JSON.parse(xhrReq.response);
+        let imageurl = a.message;
+        let image = document.getElementById('image');
+        image.setAttribute('src',imageurl);
     };
     //The boolean value is passed which is default true:- Async And for making it sync its false which we need to pass explicitly
     xhrReq.open('get','https://dog.ceo/api/breeds/image/random',true);
