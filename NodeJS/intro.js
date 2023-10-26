b738ef285201f1110e590cbce36ef6015727b615
Swagat <swagatswaroop@gmail.com>
2023-10-26 17:37:56 +0530

    Introduction Done

---
+++ NodeJS/intro.js
@@ -0,0 +1,15 @@
+console.log("Hello NodeJS");
+
+let sum = (a,b) =>{
+    return a+b;
+}
+
+//process is a global class like 
+console.log(process.argv);
+//It takes value from terminal also
+//Its first 2 values are fixes 
+//1st is the path where the node packages are installed 
+//2nd one is where the file is located path
+
+let a = process.argv.slice(2);
+console.log(sum(parseInt(a[0]),parseInt(a[1])));
