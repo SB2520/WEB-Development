f0d6c9cec1e21f89d5adea43796e7018679a9834
Swagat <swagatswaroop@gmail.com>
2023-10-27 12:12:53 +0530

    Making views in ejs

---
+++ NodeJS/express/ejs/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.port || 3000;
+let app = express();
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'depend'));
+
+app.get('/',(req,res)=>{
+    return res.render('home');
+})
+
+app.listen(port,(err)=>{
+    if(err){
+        throw new Error(err.name);
+    }
+});
