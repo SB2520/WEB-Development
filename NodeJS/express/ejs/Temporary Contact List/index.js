73f4a6468212a4f92dac65092cd5577fea3768a8
Swagat <swagatswaroop@gmail.com>
2023-10-28 12:51:32 +0530

    Added Static file

--- NodeJS/express/ejs/Temporary Contact List/index.js
+++ NodeJS/express/ejs/Temporary Contact List/index.js
@@ -1,42 +1,52 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const port = process.env.port || 3000;
 let app = express();
 //Setting up the view engine
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
-//Joing the views with the path 
-app.set('views',path.join(__dirname,'views'));
+//Joing the views with the path
+app.set("views", path.join(__dirname, "views"));
+
+//accesing the static files
+app.use(express.urlencoded());
+app.use(express.static('assests'));
 
 let arr = [
-    {
-        "Name":"Swagat",
-        "PhoneNo":992321394
-    },
-    {
-        "Name":"Aryan",
-        "PhoneNo":882313132
-    }
-]
+  {
+    name: "Swagat",
+    phone: 992321394,
+  },
+  {
+    name: "Aryan",
+    phone: 882313132,
+  }
+];
 
-app.get('/',(req,res)=>{
-    res.render('index',{
-        "title":"Home",
-    });
+app.get("/", (req, res) => {
+  return res.render("index", {
+    title: "Home",
+  });
 });
 
-app.get('/contact',(req,res)=>{
-    res.render('contact',{
-        "title":"Contacts",
-        "contactlist":arr
-    });
-})
+app.get("/contact", (req, res) => {
 
-app.listen(port,(err)=>{
-    if(err){
-        console.log(err.name);
-        throw err;
-        return;
-    }
-    console.log(`listening at port no ${port}`);
+  return res.render("contact", {
+    title: "Contacts",
+    contactlist: arr,
+  });
+});
+
+app.post('/swagat',(req,res)=>{
+    arr.push(req.body);
+    return res.redirect('/')
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err.name);
+    throw err;
+    return;
+  }
+  console.log(`listening at port no ${port}`);
 });
